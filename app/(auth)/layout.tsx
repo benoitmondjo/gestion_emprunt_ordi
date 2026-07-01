135036f69bf6759786832cb0de2554b075ab6e81
asseekomeulrichromaric-lang <asseekomeulrichromaric@gmail.com>
2026-07-01 09:14:04 +0100

    login page sans responsive

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 
 import type { Metadata } from "next";
+import "@/style/globals.css"
 
 export const metadata: Metadata = {
   title: "login",
--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,7 +1,23 @@
 
+import LeftPart from "@/components/login/LeftPart"
+import RigthPart from "@/components/login/RigthPart"
+
+
 export default function Login() {
   
-    return (<>
-    
-    </>)
+    return (
+    <main className="min-h-screen flex">
+
+        { /*Partie de gauche*/}
+
+             <LeftPart/>
+
+        {/*Partie de droite*/}
+          
+             <RigthPart/>
+            
+
+
+    </main>
+    )
 }
