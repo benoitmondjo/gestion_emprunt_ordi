91854c740b31783981cafac1b504b373dd82159d
benoitmondjo <benoitmondjo@gmail.com>
2026-07-03 13:15:38 +0100

    mise à jour du footer et dashboard

---
+++ components/Layouts/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+
+    return(
+        <>
+        <footer className="text-center text-sm text-gray-500 py-4 border-t border-gray-100 bg-white font-medium">
+            © 2026 Gestion des emprunts d'ordinateur. Tous droits réservés.
+        </footer>
+        </>
+    )
+}
