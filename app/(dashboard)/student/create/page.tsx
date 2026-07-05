ece5e61877b24e4f4a38e11f19e8dc853bcacfa0
asseekomeulrichromaric-lang <asseekomeulrichromaric@gmail.com>
2026-07-06 00:01:40 +0100

    final

--- app/(dashboard)/student/create/page.tsx
+++ app/(dashboard)/student/create/page.tsx
@@ -2,8 +2,30 @@
  * Page pour créer un nouveau apprenant 
  * @returns 
  */
-export default function CrateStudent() {
-    return (<>
-    
-    </>);
+
+
+import PageTitle from "@/components/student/create&edit/PageTitle"
+import Formulaire from "@/components/student/create&edit/Formulaire"
+import "@/style/globals.css"
+
+export default function AjouterApprenant (){
+
+    return(
+
+        <main className="p-10 bg-gray-100 min-h-screen">
+
+            {/*Titre*/}
+
+            <PageTitle 
+            title="Ajouter un apprenant"
+        
+            />
+
+            {/*Formulaire*/}
+
+            <Formulaire buttonText="Confirmer"/>
+
+
+        </main>
+    )
 }
