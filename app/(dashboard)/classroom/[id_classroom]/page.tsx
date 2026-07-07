956fdd39ba80fa726d008aa9b655c38b131db617
wen-joanel <wenetchou330@gmail.com>
2026-07-07 18:00:40 +0100

    Création de la branche feature/detail_classroom et du composant PageBreadcrumb2

--- app/(dashboard)/classroom/[id_classroom]/page.tsx
+++ app/(dashboard)/classroom/[id_classroom]/page.tsx
@@ -1,9 +1,21 @@
+// On importe les composants 
+import PageBreadcrumb2 from "@/components/classroom/PageBreadcrumb2";
+
+
+
+
 /**
- * Page pour voir les details d'une classe
+* Page pour voir les details d'une classe
  * @returns 
  */
 export default function ShowClassroom() {
     return (<>
-    
+        <PageBreadcrumb2
+            items={[
+                { label: "Salles", href: "/dashboard/classroom" },
+                { label: "Détail de la salle" },
+            ]}
+        />
+
     </>);
 }
